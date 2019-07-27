5d252bb6a85430ae2264dd347eeeddc2c4bdeaf1
yocxli <yocxli@gmail.com>
2019-07-27 21:35:55 +0900

    Refactor unit tests

--- app/src/test/java/yocxli/gallerysample/data/MediaRepositoryImplTest.kt
+++ app/src/test/java/yocxli/gallerysample/data/MediaRepositoryImplTest.kt
@@ -3,30 +3,28 @@ package yocxli.gallerysample.data
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-
 import org.junit.runner.RunWith
-import android.provider.MediaStore
-import android.database.DataSetObserver
-import android.database.ContentObserver
-import org.robolectric.Shadows.shadowOf
-import org.robolectric.fakes.RoboCursor
-import java.util.*
+import org.mockito.Mock
 
 
 @RunWith(AndroidJUnit4::class)
 class MediaRepositoryImplTest {
-    private val context: Context = ApplicationProvider.getApplicationContext<Context>()
+    private val context: Context = ApplicationProvider.getApplicationContext()
 
     lateinit var repository: MediaRepositoryImpl
 
+    @Mock
+    lateinit var mockLocalDataStore: MediaDataStore
+
+    @Mock
+    lateinit var mockRemoteDataStore: MediaDataStore
 
     @Before
     fun setUp() {
-        repository = MediaRepositoryImpl(context)
+        repository = MediaRepositoryImpl(mockLocalDataStore, mockRemoteDataStore)
     }
 
     @After
@@ -35,36 +33,36 @@ class MediaRepositoryImplTest {
 
     @Test
     fun listAll() {
-        val cursor = object : RoboCursor() {
-            override fun registerContentObserver(observer: ContentObserver?) {
-                // no op
-            }
-
-            override fun unregisterContentObserver(observer: ContentObserver?) {
-                // no op
-            }
-
-            override fun registerDataSetObserver(observer: DataSetObserver?) {
-                // no op
-            }
-
-            override fun unregisterDataSetObserver(observer: DataSetObserver?) {
-                // no op
-            }
-
-            override fun isClosed(): Boolean {
-                return true
-            }
-        }
-        cursor.setColumnNames(
-            Arrays.asList(
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME
-            )
-        )
-        cursor.setResults(arrayOf(arrayOf(1L, "WhatsApp"), arrayOf(2L, "Photos"), arrayOf(3L, "WhatsApp")))
-        shadowOf(context.contentResolver)
-            .setCursor(MediaStore.Files.getContentUri("external"), cursor)
+//        val cursor = object : RoboCursor() {
+//            override fun registerContentObserver(observer: ContentObserver?) {
+//                // no op
+//            }
+//
+//            override fun unregisterContentObserver(observer: ContentObserver?) {
+//                // no op
+//            }
+//
+//            override fun registerDataSetObserver(observer: DataSetObserver?) {
+//                // no op
+//            }
+//
+//            override fun unregisterDataSetObserver(observer: DataSetObserver?) {
+//                // no op
+//            }
+//
+//            override fun isClosed(): Boolean {
+//                return true
+//            }
+//        }
+//        cursor.setColumnNames(
+//            Arrays.asList(
+//                MediaStore.Images.Media._ID,
+//                MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+//            )
+//        )
+//        cursor.setResults(arrayOf(arrayOf(1L, "WhatsApp"), arrayOf(2L, "Photos"), arrayOf(3L, "WhatsApp")))
+//        shadowOf(context.contentResolver)
+//            .setCursor(MediaStore.Files.getContentUri("external"), cursor)
 //        val result = repository.listAll()
 //        assertThat(result.value?.size).isEqualTo(0)
     }
